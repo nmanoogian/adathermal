91dfc0daf3d1d97bc9c9fee7354e2eeb580ce935
Nic Manoogian <nmanoogian@gmail.com>
2019-02-27 11:52:27 -0500

    Fix bbcode failed tags

--- adapters/bbcodeadapter.py
+++ adapters/bbcodeadapter.py
@@ -31,19 +31,17 @@ class BBCodeAdapter:
 
     def print(self, raw):
         parser = bbcode.Parser()
-        parser.add_simple_formatter("m", "%(value)s")
-        parser.add_simple_formatter("l", "%(value)s")
+        for tag in BBCodeAdapter.tags:
+            parser.add_simple_formatter(tag.name, "%(value)s")
         tokens = parser.tokenize(raw)
         for token in tokens:
             token_type, tag_name, _, token_text = token
-            if token_type == bbcode.Parser.TOKEN_DATA:
+            tag = next((t for t in BBCodeAdapter.tags if t.name == tag_name), None)
+            if token_type == bbcode.Parser.TOKEN_DATA or tag is None:
                 self.printer.print(token_text)
             elif token_type == bbcode.Parser.TOKEN_NEWLINE:
                 self.printer.print("\n")
             else:
-                tag = next((t for t in BBCodeAdapter.tags if t.name == tag_name), None)
-                if tag is None:
-                    raise ValueError("{} is not a valid tag".format(tag_name))
                 if token_type == bbcode.Parser.TOKEN_TAG_START:
                     tag.on_open(self.printer)
                 else:
